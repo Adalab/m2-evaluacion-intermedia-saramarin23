19a05f65579528a68d785fd8a0b1a8526d86c9a6
saramarin23 <saramarin.econ@gmail.com>
2019-07-29 15:43:57 +0200

    add eventListener and sum of tries

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
 "use strict";
+
+const button = document.querySelector(".btn");
+const input = document.querySelector(".input");
+const instructions = document.querySelector(".instructions__number");
+const tries = document.querySelector(".tries");
+tries.innerHTML = 0;
+
+function guessNumber() {
+  debugger;
+  tries.innerHTML++;
+  const randomNumber = Math.floor(Math.random * 100);
+  const userNumber = input.value;
+  if (userNumber < randomNumber) {
+    instructions.innerHTML = "Demasiado bajo";
+  } else if (userNumber > randomNumber) {
+    instructions.innerHTML = "Demasiado alto";
+  } else if (userNumber === randomNumber) {
+    instructions.innerHTML = "¡HAS GANADO, CAMPEONA!";
+  } else {
+    return "Error";
+  }
+  console.log(randomNumber);
+}
+
+button.addEventListener("click", guessNumber);
