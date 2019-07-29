ae6e2c3cfb6d0577ebc12811d7043d0c466483c4
saramarin23 <saramarin.econ@gmail.com>
2019-07-29 16:48:35 +0200

    finish exercise

--- js/main.js
+++ js/main.js
@@ -4,22 +4,32 @@ const button = document.querySelector(".btn");
 const input = document.querySelector(".input");
 const instructions = document.querySelector(".instructions__number");
 const tries = document.querySelector(".tries");
+const randomNumber = Math.ceil(Math.random() * 100);
+
 tries.innerHTML = 0;
 
 function guessNumber() {
-  debugger;
   tries.innerHTML++;
-  const randomNumber = Math.floor(Math.random * 100);
-  const userNumber = input.value;
-  if (userNumber < randomNumber) {
+  //debugger;
+  const userNumber = parseInt(input.value);
+  if (userNumber < randomNumber && userNumber >= 1 && userNumber <= 100) {
     instructions.innerHTML = "Demasiado bajo";
-  } else if (userNumber > randomNumber) {
+  } else if (
+    userNumber > randomNumber &&
+    userNumber >= 1 &&
+    userNumber <= 100
+  ) {
     instructions.innerHTML = "Demasiado alto";
-  } else if (userNumber === randomNumber) {
+  } else if (
+    userNumber === randomNumber &&
+    userNumber >= 1 &&
+    userNumber <= 100
+  ) {
     instructions.innerHTML = "¡HAS GANADO, CAMPEONA!";
   } else {
-    return "Error";
+    instructions.innerHTML = "Prueba un número entre 1 y 100";
   }
+  console.log(userNumber);
   console.log(randomNumber);
 }
 
