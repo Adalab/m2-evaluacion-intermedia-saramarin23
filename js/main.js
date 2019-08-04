fa593ef360fb09a2f7ed45ee47eb5bb8be3f99b8
saramarin23 <saramarin.econ@gmail.com>
2019-08-04 12:47:07 +0200

    fix javascript issues

--- js/main.js
+++ js/main.js
@@ -1,36 +1,41 @@
 "use strict";
 
-const button = document.querySelector(".btn");
-const input = document.querySelector(".input");
-const instructions = document.querySelector(".instructions__number");
-const tries = document.querySelector(".tries");
+const button = document.querySelector(".js-btn");
+const input = document.querySelector(".js-input");
+const instructions = document.querySelector(".js-instructions__number");
+const tries = document.querySelector(".js-tries");
 const randomNumber = Math.ceil(Math.random() * 100);
 
-tries.innerHTML = 0;
+let triesNumber = 0;
 
-function guessNumber() {
-  tries.innerHTML++;
-  //debugger;
-  const userNumber = parseInt(input.value);
-  if (userNumber < randomNumber && userNumber >= 1 && userNumber <= 100) {
-    instructions.innerHTML = "Demasiado bajo";
-  } else if (
-    userNumber > randomNumber &&
-    userNumber >= 1 &&
-    userNumber <= 100
-  ) {
-    instructions.innerHTML = "Demasiado alto";
-  } else if (
-    userNumber === randomNumber &&
-    userNumber >= 1 &&
-    userNumber <= 100
-  ) {
-    instructions.innerHTML = "¡HAS GANADO, CAMPEONA!";
-  } else {
-    instructions.innerHTML = "Prueba un número entre 1 y 100";
-  }
-  console.log(userNumber);
-  console.log(randomNumber);
+function update(event) {
+  event.preventDefault();
+  showFeedback();
+  showCounter();
 }
 
-button.addEventListener("click", guessNumber);
+function showCounter() {
+  triesNumber++;
+  tries.innerHTML = triesNumber;
+}
+
+const writeInstruction = function(Text) {
+  instructions.innerHTML = Text;
+};
+
+function showFeedback() {
+  const userNumber = parseInt(input.value);
+  if (userNumber < 1 || userNumber > 100) {
+    writeInstruction("Prueba un número entre 1 y 100");
+  } else if (userNumber > randomNumber) {
+    writeInstruction("Demasiado alto");
+  } else if (userNumber === randomNumber) {
+    writeInstruction("¡HAS GANADO, CAMPEONA!");
+  } else {
+    writeInstruction("Demasiado bajo");
+    console.log("Número introducido: ", userNumber);
+    console.log("Número ganador: ", randomNumber);
+  }
+}
+
+button.addEventListener("click", update);
